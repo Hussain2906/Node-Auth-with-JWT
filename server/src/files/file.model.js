4d72b2ef954761db802f759594035e9c34eb383e
Hussain2906 <huss0602029@gmail.com>
2025-09-10 20:45:21 +0530

    mongoose model changes

--- server/src/files/file.model.js
+++ server/src/files/file.model.js
@@ -2,23 +2,24 @@ const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 const fileSchema = new Schema(
-    {
-        originalName: { type: String, required: true },          
-        mimeType: { type: String, required: true },          
-        sizeBytes: { type: Number, required: true },          
-        storage: {
-            kind: { type: String, enum: ["disk", "s3"], default: "disk" }, 
-            path: { type: String},                   
-            url: { type: String },                                   
-        },
-        qr: {
-            pngPath: { type: String },           
-            publicUrl: { type: String},          
-        },
-        uploaderId: { type: String, required: true },
+  {
+    originalName: { type: String, required: true },
+    mimeType: { type: String, required: true },
+    sizeBytes: { type: Number, required: true },
+    storage: {
+      kind: { type: String, enum: ["disk", "s3"], default: "s3" },
+      key: { type: String },   // S3 object key or local path
+      url: { type: String },   // public URL
     },
-    { timestamps: true }
-)
+    qr: {
+      key: { type: String, required: true },       //  matches service code
+      publicUrl: { type: String, required: true },
+    },
+    uploaderId: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
 fileSchema.index({ createdAt: -1 });
 fileSchema.index({ mimeType: 1 });
 
